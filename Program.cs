af116f4cf3882935b3cbca7d95e22ba5e52bbcef
Joseph <Jpatrick148@Gmail.com>
2018-10-10 16:37:05 -0400

    Completed

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace Lab_5
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please enter an Integer from 1 to 10");
+            int num = int.Parse(Console.ReadLine());
+
+            long fact = Factorial(num);
+
+            Console.WriteLine("The Factorial of {0} is {1}", num, fact);
+
+            Console.ReadLine();
         }
+
+        public static long Factorial(int i)
+        {
+
+            if (i == 0)
+            {
+                return 1;
+            }
+
+            return i * Factorial(i - 1);
+        }
+        
     }
 }
